b583e03e0108dec32a45746937991511049fc9fd
masdefry <muhammaddefryan@gmail.com>
2023-11-08 14:23:17 +0700

    Buy Item Market Pro

---
+++ Exercise/Database.js
@@ -0,0 +1,18 @@
+class Products{
+    constructor(name, price, stock, unit){
+        this.name = name 
+        this.price = price 
+        this.stock = stock 
+        this.unit = unit
+    }
+}
+
+const dbProducts = [
+    new Products('Lobak', 10000, 100, 'Kg'),
+    new Products('Kol', 5000, 50, 'Kg'),
+    new Products('Sawi', 3000, 30, 'Ikat')
+]
+
+const dbCarts = [
+    
+]
